846aaac82c403c1faafde4b00202989f5247dfe4
Vitalii <cultivateweb@gmail.com>
2019-05-11 06:34:49 +0300

    style

--- src/app/token/token.component.ts
+++ src/app/token/token.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TelegramBotApiService, Update, User } from '../telegram-bot-api.service';
 import { MatSlideToggleChange } from '@angular/material';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'tb-token',
   template: `
-
   <div class="token">  
     <mat-form-field class="token-input" *ngIf="!activated else elseBlock">
       <input matInput placeholder="Bot token" [(ngModel)]="token">
@@ -13,23 +13,16 @@ import { MatSlideToggleChange } from '@angular/material';
     <ng-template #elseBlock>
       <span class="token-name">{{'@' + user.username + ' (' + user.first_name + ')'}}</span>
     </ng-template>
-
     <span class="spacer"></span>
-    
     <mat-slide-toggle color="warn" 
                       (change)="activate($event)"
                       [(ngModel)]="activated">Activate</mat-slide-toggle>
-  </div>
-
-  `,
+  </div>`,
   styles: [ `
     .token { display: flex; align-items: center }
-
     .token-input { flex: 1 }
     .token-name { flex: 1 1 20% }
-
     .spacer { flex: 1 1 auto }
-
   ` ] })
 export class TokenComponent implements OnInit {
   activated: boolean = false
@@ -41,7 +34,13 @@ export class TokenComponent implements OnInit {
   activate(event: MatSlideToggleChange) {    
     if (event.checked) {
       this.telegram.getMe(this.token)
-                   .subscribe((user: User) => this.user = user)
+                  //  .pipe()
+                   .subscribe(
+                     (user: User) => this.user = user,
+                     (error: HttpErrorResponse) => {
+                       // {"ok":false,"error_code":404,"description":"Not Found"}
+                       this.activated = false
+                     })
       //  this.activated = false
     }
   }
